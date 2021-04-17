f098b1974e6e35b0929cf209f52728e33af16fd0
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-04-17 18:43:50 +0300

    Assigning new speed

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
     $("#hide1").hide();
     $("#design").click(function () {
-        $("#hide1").toggle(500);
+        $("#hide1").toggle(700);
     });
     $("#show").click(function () {
-        $("#hide1").show(500);
+        $("#hide1").show(700);
     });
 
     $("#hide2").hide();
     $("#development").click(function () {
-        $("#hide2").toggle(500);
+        $("#hide2").toggle(700);
     });
     $("#show").click(function () {
-        $("#development").show(500);
+        $("#development").show(700);
     });
     $("#hide3").hide();
     $("#products-management").click(function () {
         $("#hide3").toggle(500);
     });
     $("#show").click(function () {
-        $("#products-management").show(500);
+        $("#products-management").show(700);
     });
     preventDefault();
 })
