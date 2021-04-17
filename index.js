c78341ab041be6556d05ec76d11df0c8086ac802
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-04-17 17:51:04 +0300

    Adding toggle to what we do

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 $(document).ready(function () {
-    $("p").hide();
+    $("#hide1").hide();
     $("#design").click(function () {
-        $("#design-p").toggle();
+        $("#hide1").toggle(500);
+    });
+    $("#show").click(function () {
+        $("#hide1").show(500);
+    });
+
+    $("#hide2").hide();
+    $("#development").click(function () {
+        $("#hide2").toggle();
+    });
+    $("#show").click(function () {
+        $("#development").show();
+    });
+    $("#hide3").hide();
+    $("#products-management").click(function () {
+        $("#hide3").toggle();
+    });
+    $("#show").click(function () {
+        $("#products-management").show();
     });
     preventDefault();
 })
