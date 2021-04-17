3e2fec8cab43f48e0e51e2161466ea0615807578
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-04-17 17:54:41 +0300

    Making some slight changes

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ $(document).ready(function () {
 
     $("#hide2").hide();
     $("#development").click(function () {
-        $("#hide2").toggle();
+        $("#hide2").toggle(500);
     });
     $("#show").click(function () {
-        $("#development").show();
+        $("#development").show(500);
     });
     $("#hide3").hide();
     $("#products-management").click(function () {
-        $("#hide3").toggle();
+        $("#hide3").toggle(500);
     });
     $("#show").click(function () {
-        $("#products-management").show();
+        $("#products-management").show(500);
     });
     preventDefault();
 })
